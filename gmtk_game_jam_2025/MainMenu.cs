77a5d58b2cb782757cf15b13fbca89dc47c9f825
kongshijatwit <JKongShi@gmail.com>
2025-07-31 10:14:47 -0400

    Finish main menu

--- gmtk_game_jam_2025/MainMenu.cs
+++ gmtk_game_jam_2025/MainMenu.cs
@@ -2,9 +2,21 @@ using Godot;
 
 public partial class MainMenu : Control
 {
+	private Control _settingsMenu;
+
+	public override void _Ready()
+	{
+		_settingsMenu = GetNode<Control>("Settings");
+	}
+
 	public void OnPlayButtonPressed()
 	{
 		GD.Print("Play button pressed");
+	}
+
+	public void OnSettingsButtonPressed()
+	{
+		_settingsMenu.Visible = true;
 	}
 
 	public void OnQuitButtonPressed()
