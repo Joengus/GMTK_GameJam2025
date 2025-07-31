d40bdbb9c4bf61eda3cfc1463ec83d6a27c00c20
kongshijatwit <JKongShi@gmail.com>
2025-07-31 00:31:17 -0400

    Add audio bus

--- gmtk_game_jam_2025/MainMenu.cs
+++ gmtk_game_jam_2025/MainMenu.cs
@@ -5,10 +5,18 @@ public partial class MainMenu : Control
 	public static void OnPlayButtonPressed()
 	{
 		GD.Print("Play button pressed");
+		// AudioServer.GetBusIndex("Master");
 	}
 
 	public void OnQuitButtonPressed()
 	{
 		GetTree().Quit();
+	}
+
+	public void OnVolumeSliderChanged(float value)
+	{
+		AudioServer.SetBusVolumeLinear(AudioServer.GetBusIndex("Master"), value);
+		GD.Print("Master Decibels: " + AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex("Master")));
+		GD.Print("Master Linear: " + AudioServer.GetBusVolumeLinear(AudioServer.GetBusIndex("Master")));
 	}
 }
