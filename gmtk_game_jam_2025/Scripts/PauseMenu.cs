2b9fb561615577d7740379495dee146b3a303f9a
kongshijatwit <JKongShi@gmail.com>
2025-08-02 00:37:09 -0400

    Create tasks

---
+++ gmtk_game_jam_2025/Scripts/PauseMenu.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public partial class PauseMenu : Control
+{
+	[Export] public PackedScene MainMenuScene;
+
+	public void OnResumeButtonPressed()
+	{
+		Visible = false;
+	}
+
+	public void OnMainMenuButtonPressed()
+	{
+		GetTree().ChangeSceneToPacked(MainMenuScene);
+	}
+}
