ce590b1acaac8632a47691af205b2e88253f0735
kongshijatwit <JKongShi@gmail.com>
2025-08-03 10:01:43 -0400

    Finish flavor text

--- gmtk_game_jam_2025/Scripts/Task/TaskManager.cs
+++ gmtk_game_jam_2025/Scripts/Task/TaskManager.cs
@@ -3,11 +3,19 @@ using Godot;
 public partial class TaskManager : Node3D
 {
 	public static TaskManager Instance;
+
+	private const int NON_NEUTRAL_FLAVOR_TEXT_POINT_THRESHOLD = 50;
 	[Export] public TaskResource[] TaskList;
+	private RichTextLabel _flavorText;
 
 	public override void _EnterTree()
 	{
 		Instance ??= this;
+	}
+
+	public override void _Ready()
+	{
+		_flavorText = GetNode<RichTextLabel>("Control/Text");
 	}
 
 	public void CompleteTask(TaskResource taskToMarkComplete)
@@ -19,6 +27,7 @@ public partial class TaskManager : Node3D
 			if (!t.IsComplete) return;
 		}
 		task.IsComplete = true;
+		DisplayFlavorText(taskToMarkComplete);
 	}
 
 	private TaskResource FindTask(TaskResource taskToFind)
@@ -30,5 +39,21 @@ public partial class TaskManager : Node3D
 		}
 		GD.PrintErr($"Could not find task '{taskToFind.TaskName}'.");
 		return null;
+	}
+
+	private void DisplayFlavorText(TaskResource task)
+	{
+		if (GameManager.Instance.CitizenPoints >= NON_NEUTRAL_FLAVOR_TEXT_POINT_THRESHOLD)
+		{
+			_flavorText.Text = task.DescriptionCitizen;
+		}
+		else if (GameManager.Instance.RebelPoints >= NON_NEUTRAL_FLAVOR_TEXT_POINT_THRESHOLD)
+		{
+			_flavorText.Text = task.DescriptionRebel;
+		}
+		else
+		{
+			_flavorText.Text = task.DescriptionNeutral;
+		}
 	}
 }
