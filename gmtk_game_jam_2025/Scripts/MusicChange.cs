deca582c857eb04a60f07c3b5bf806ada60dd164
kongshijatwit <JKongShi@gmail.com>
2025-08-04 21:34:08 -0400

    Finish music crossfading

--- gmtk_game_jam_2025/Scripts/MusicChange.cs
+++ gmtk_game_jam_2025/Scripts/MusicChange.cs
@@ -2,13 +2,27 @@ using Godot;
 
 public partial class MusicChange : Area3D
 {
-	[Export] private Music _music;
+	[Export] private Music _musicToPlay;
+	private AudioStreamPlaybackInteractive _musicPlayer;
+
+	public override void _Ready()
+	{
+		_musicPlayer = (AudioStreamPlaybackInteractive)GetNode<AudioStreamPlayer>("../MusicPlayer").GetStreamPlayback();
+	}
 
 	public void OnCollisionEnter(Node3D other)
 	{
 		if (other.GetParent().Name == "PlayerCharacter")
 		{
-			GetNode<MusicManager>("../MusicPlayer").ChangeMusic(_music);
+			if (_musicPlayer.GetCurrentClipIndex() == (int)_musicToPlay) return;
+			_musicPlayer.SwitchToClip((int)_musicToPlay);
 		}
 	}
 }
+
+public enum Music
+{
+	CLOSET,
+	OFFICE,
+	EXECUTIVE
+};
